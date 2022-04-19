3f5cff291fa3ff3517c929e89e2f074d3bf3bb91
jlindner22 <jenlindner22@gmail.com>
2022-04-18 22:04:06 -0400

    purchase working, end of night commit

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 'use strict';
 
-// const switcher = document.querySelector('.btn');
-// switcher.addEventListener('click', function() {
-//     document.body.classList.toggle('dark-theme')
-//     appboy.logCustomEvent("changed-theme")
-//     let className = document.body.className;
-//     if(className == "light-theme") {
-//         this.textContent = "Dark";
-//     }
-//     else {
-//         this.textContent = "Light";
-//     }
-//     // console.log('current class name: ' + className);
-// });
-
-//signup form submission
 const submitForm = () => {
     const text = document.getElementById("signup");
     const email = document.getElementById("email");
@@ -24,9 +9,7 @@ const submitForm = () => {
     // text.textContent = appboy.getDeviceId(function(deviceId) {deviceId}) //print device ID/user ID on page
     appboy.changeUser(email.value)
     appboy.getUser().setEmail(email.value)
-    appboy.getUser().setFirstName(name.value)
     setTimeout(function(){ text.reset() }, 2000)
-    // appboy.getUser().setGender() //need logic
     console.log(appboy.getUser().getUserId(x => console.log(x)))
     console.log("form submitted!")
 }
@@ -70,46 +53,15 @@ pushPromptButton.addEventListener("click", submitPushPrompt);
 const submitPurchaseForm = () => {
     const size = document.getElementById("cake-size");
     const flavor = document.getElementById("cake-flavor");
-    console.log(size.value)
-    console.log(flavor.value)
-    appboy.logPurchase("Cake", 15, 1, {"Size": size.value, "Flavor": flavor.value})
+    console.log(size.value,flavor.value)
+    appboy.logPurchase("Cake",15,"USD",1, {"Size": size.value, "Flavor": flavor.value})
+    const text = document.getElementById("cart");
+    text.textContent = `Item: 1 Cake, ${flavor.value}, ${size.value}`
+    // appboy.logCustomEvent("Added to Cart")
 }
 
 const purchaseButton = document.getElementById("cake-button")
-purchaseButton.addEventListener("click", submitPurchaseForm);
-// let openModal = document.getElementsByClassName("flex-container-row");
-// let modal = document.getElementsByClassName("aromodal-body");
-// let span = document.getElementsByClassName("modal-close");
-// // [0];
-
-// // When the user clicks on the button, open the modal
-// openModal.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-
-
-// const addToCartBtn = document.getElementsByClassName("single_add_to_cart_button");
-//     const attributeValue = document.getElementById("attribute-value");    
-//     const cartBox = document.getElementById("fdoe_mini_cart_id");
-    // cartBox.textContent = 
-
-/* <div class="fdoe_mini_cart" id="fdoe_mini_cart_id"></div> */
-
-// class="wc-pao-addon-field wc-pao-addon-select"
-// class="wc-pao-addon-field wc-pao-addon-select"
+purchaseButton.addEventListener("submit", submitPurchaseForm);
 
 
 //add to the Your Order section
@@ -126,27 +78,16 @@ purchaseButton.addEventListener("click", submitPurchaseForm);
 
 
 
-
-// Get the modal
+// Cake modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
